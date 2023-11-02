75a64c30ae205df042de25137c6ea7ae98616197
NilotpalM <128976944+Nilvalox@users.noreply.github.com>
2023-11-03 00:01:55 +0530

    Add files via upload

---
+++ edge_trig_flipflop/edge_trig_flipflop.srcs/sources_1/new/edge_triggered.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12.10.2023 13:03:25
+// Design Name: 
+// Module Name: edge_triggered
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module edge_triggered(
+
+    );
+endmodule
